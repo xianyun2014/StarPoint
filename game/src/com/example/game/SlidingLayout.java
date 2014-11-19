5909bdecc4d6d26227546b5b6231e84cba095ffd
xianyun2014 <xianyun1230@gmail.com>
2014-11-19 22:05:26 +0800

    V0.1

---
+++ game/src/com/example/game/SlidingLayout.java
@@ -0,0 +1,13 @@
+package com.example.game;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class SlidingLayout extends RelativeLayout {
+
+	public SlidingLayout(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+}
